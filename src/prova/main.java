cb87f1f27bb3372cd1b393b4ce0b1a02858cd906
FELIPE ARAUJO LINS <20162d13gr0099@LABS.GARANHUNS.IFPE>
2018-12-07 22:00:43 -0300

    atualizando

--- src/prova/main.java
+++ src/prova/main.java
@@ -16,15 +16,77 @@ public class main {
 		
 		 ArrayList<Professor>professores = new ArrayList<>();
 		
-		 String pnome= JOptionPane.showInputDialog("digite o nome");
-		 int pcodigo = Integer.parseInt(JOptionPane.showInputDialog("digite o codigo"));
+		 
+	     int op;
 		 
 		 
-		 p =new Professor(pnome,pcodigo);
-		professores.add(p);
-		
-		
+		        do {
+		        	
+		        op= Integer.parseInt(JOptionPane.showInputDialog("digite 1 para cadastra \n 2 para listar \n 3 para remover \n 0 para sair"));
+		        
+		        switch (op  ) {
+		        
+		        case 1 :
+               boolean tem=false;
+		        	
+		        	String pnome= JOptionPane.showInputDialog("digite o nome");
+		   		   int pcodigo = Integer.parseInt(JOptionPane.showInputDialog("digite o codigo"));
+					
+		   		   for(Professor pr : professores) {
+		   		   
+				   		     if(pr.getCodigo() ==pcodigo) {
+				   			   
+				   		        tem=true;
+							
+				   		    op=0;
+							
+				   		    break;
+					   		
+				   		      } 
+		   		   } 
+		   		   
+					   		   if(tem==false) {
+					   			   
+					   			   p= new Professor(pnome,pcodigo);
+					   			   professores.add(p);
+					   			   
+					   		   }else {
+					   			   
+					   			   System.out.print("Ja existe esse codigo");
+					   			   op=0;
+					   		   }
+					   		   
+					   		   
+		         break;
+		    
+		        case 2 :
 
+		        	 for(Professor pr : professores) {
+		     			
+		 			    System.out.println("nome: "+pr.getNome()+"\ncodigo: "+pr.getCodigo());
+		 			
+		 		          }
+		        	
+		         break;
+		       
+		        case 3 :
+                
+			        	
+		        	
+		        	
+		        	for(int i=0;i<professores.size();i++) {
+						 
+			        	 professores.remove(i);
+			        	}
+		         break;
+		        
+		        
+		        }
+		
+		        } while(op !=0);
+		        
+		        
+		        
 	}
 
 }
